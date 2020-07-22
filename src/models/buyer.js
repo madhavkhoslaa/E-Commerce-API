3c8bb9c3587a03a0cab19db9e43cc720c4a1caeb
Madhav Khosla <madhav.khoslaa@gmail.com>
2020-07-22 21:05:58 +0530

    buyer auth functions

--- src/models/buyer.js
+++ src/models/buyer.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 
 BuyerSchema = mongoose.Schema({
     name: {
@@ -23,14 +25,45 @@ BuyerSchema = mongoose.Schema({
         type: String,
         required: true,
         trim: true
-    }
+    },
+    tokens: [{
+        tokens: {
+            type: String,
+            required: true
+        }
+    }]
 })
 
 BuyerSchema.pre('save', async function(next) {
-    const user = this.user
-    if (user.isModified('password')) user.password = bcrypt.hashSync(user.password, 8)
+    const buyer = this
+    if (buyer.isModified('password')) user.password = bcrypt.hashSync(buyer.password, 8)
     next()
 })
+
+BuyerSchema.methods.toJSON = function() {
+    const buyer = this
+    const buyer = buyer.toObject()
+    delete buyer.password
+    delete buyer.tokens
+    return buyer
+}
+BuyerSchema.methods.getAuthTokens = async function() {
+    const buyer = this
+    const token = jwt.sign({ _id: buyer._id.toString() }, 'password')
+    buyer.token = buyer.token.concat({ token })
+    try {
+        await buyer.save()
+    } catch (e) {
+        throw new Error('Cannot Create Token')
+    }
+}
+BuyerSchema.statics.FindByCredentials = async function(email, password) {
+    buyer = await Buyer.findOne({ email, passpwrd })
+    if (!buyer) throw new Error('unable to login')
+    isMatch = await bcrypt.compare(buyer.password, password)
+    if (!isMatch) throw new Error('unable to login')
+    return buyer
+}
 Buyer = mongoose.model('buyer', BuyerSchema)
 
 module.exports = Buyer
