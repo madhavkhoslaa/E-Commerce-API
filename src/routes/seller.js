1360791b045fae6854b4b865753dcf89ea2c62aa
Madhav Khosla <madhav.khoslaa@gmail.com>
2020-07-23 14:19:42 +0530

    CRUD operations for seller

--- src/routes/seller.js
+++ src/routes/seller.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Seller = require('../models/seller')
+const Auth = require('../middleware/Sellerauth')
 
 const SellerRouter = express.Router()
 
@@ -14,6 +15,42 @@ SellerRouter.post('/register/seller', async(req, res) => {
     }
 })
 
+SellerRouter.get('/seller/me', Auth, async(req, res) => {
+    try {
+        const seller = req.seller
+        res.send(seller)
+    } catch (e) {
+        res.status(500)
+    }
+})
 
+SellerRouter.patch('/seller/me', Auth, async(req, res) => {
+    const to_update = Object.keys(req.body)
+    const valid_keys = ['name', 'email', 'password']
+    const is_valid = to_update.every((update) => valid_keys.includes(update))
+    if (!is_valid) res.status(400).send({ message: "cannot update" })
+    try {
+        const seller = req.seller
+        to_update.forEach(update => seller[update] = req.body[update])
+        await seller.save()
+        if (!seller) return res.send("s")
+        res.status(200).send(seller)
+    } catch (e) {
+        res.status(500)
+    }
+})
+
+SellerRouter.delete('/seller/me', Auth, async(req, res) => {
+    const seller = req.seller
+    seller.remove()
+})
+
+SellerRouter.post('/seller/login', async(req, res) => {
+    res.send("too")
+})
+
+SellerRouter.post('/seller/logout', async(req, res) => {
+    res.send("too")
+})
 
 module.exports = SellerRouter
