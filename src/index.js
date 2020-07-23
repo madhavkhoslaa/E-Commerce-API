e69df3d8911b341c27d221a17cf0e306f4329c12
Madhav Khosla <madhav.khoslaa@gmail.com>
2020-07-23 12:57:59 +0530

    used express middlewares

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const SellerRoute = require('./routes/seller')
+require('./db/connection')
 const app = express()
 
+app.use(express.json())
+app.use(SellerRoute)
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
