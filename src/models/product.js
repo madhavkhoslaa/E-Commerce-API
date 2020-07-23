8eb61d8202e4dedf68f8ece3f30a80bb24c836e0
Madhav Khosla <madhav.khoslaa@gmail.com>
2020-07-23 10:08:18 +0530

    models for products

---
+++ src/models/product.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const validator = require('validator')
+const mongoose = require('mongoose')
+
+ProductSchema = mongoose.Schema({
+    item_name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'seller'
+    }
+})
+
+Product = mongoose.model('product', ProductSchema)
+
+module.exports = Product
