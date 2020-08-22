44505b6323d192e87479959e947b4154a716810f
Madhav Khosla <madhav.khoslaa@gmail.com>
2020-08-23 01:52:15 +0530

    more informative errors for failing cases

--- src/routes/buyer.js
+++ src/routes/buyer.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const Product = require('../models/product')
 const Buyer = require('../models/buyer')
+const Seller = require('../models/seller')
 const Auth = require('../middleware/Buyerauth')
 const Transaction = require('../models/transaction')
+const {
+    Mongoose
+} = require('mongoose')
 const BuyerRouter = express.Router()
 
 BuyerRouter.post('/buyer/register', async (req, res) => {
@@ -67,17 +71,18 @@ BuyerRouter.delete('/buyer/me', Auth, async (req, res) => {
         const buyer = await Buyer.deleteOne({
             _id: req.buyer._id
         })
-        if (!buyer) return res.send({
-            message: "unable to delete"
+        if (!buyer) throw ({
+            message: {
+                message: "unable to delete buyer because cannot find in databse"
+            },
+            https_code: 400
         })
         res.status(200).send({
             message: "buyer deleted",
             buyer
         })
-    } catch (e) {
-        res.status(500).send({
-            message: "internal server error"
-        })
+    } catch (error) {
+        res.status(error.https_code).send(error.message)
     }
 })
 
@@ -98,41 +103,65 @@ BuyerRouter.post('/buyer/logout', Auth, async (req, res) => {
 BuyerRouter.post('/buyer/login', async (req, res) => {
     try {
         const buyer = await Buyer.FindByCredentials(req.body.email, req.body.password)
-        if (!buyer) return res.status(400).send({
-            message: "unable to login"
+        if (!buyer) throw ({
+            message: {
+                message: "check email or username"
+            },
+            https_code: 400
         })
         const token = await buyer.getAuthToken()
         res.status(200).send({
             buyer,
             token
         })
-    } catch (e) {
-        console.log(e)
-        res.status(500).send({
-            message: "internal server error"
+        if (!token) throw ({
+            message: {
+                message: "unable to login"
+            },
+            https_code: 500
         })
+    } catch (error) {
+        res.status(error.https_code).send(error.message)
     }
 })
 
 BuyerRouter.post('/buyer/buy/:id', Auth, async (req, res) => {
-    const isTransacted = await Transaction.findOne({
-        product_id: req.params.id
-    })
-    if (isTransacted) return res.status(400).send({
-        message: `cannot buy this product of id: ${req.params.id}`
-    })
-    const product = await Product.findById(req.params.id)
-    if (!product) return res.status(404).send({
-        message: `product with id:${req.params.id} not found`
-    })
-    const transaction = new Transaction({
-        seller_id: product.owner.toString(),
-        buyer_id: req.buyer._id,
-        product_id: product._id
-    })
-    await transaction.save()
-    await product.remove()
-    res.status(200).send(transaction)
-
+    try {
+        const product = await Product.findById(req.params.id)
+        if (!product) throw ({
+            message: {
+                message: `cannot find product with given id: ${req.params.id}`
+            },
+            https_code: 400
+        })
+        const isTransacted = await Transaction.findOne({
+            product_id: req.params.id
+        })
+        if (isTransacted) throw ({
+            message: {
+                message: `cannot find purchase with given id: ${req.params.id}`
+            },
+            https_code: 400
+        })
+        const seller = await Seller.findById(product.owner)
+        const transaction = new Transaction({
+            seller_id: product.owner,
+            buyer_id: Mongoose.Schema.Types.ObjectId(req.buyer._id),
+            product_id: Mongoose.Schema.Types.ObjectId(product._id)
+        })
+        await transaction.save()
+        await product.remove()
+        res.status(200).send({
+            message: "transaction complete",
+            details: {
+                buyer: req.buyer.name,
+                seller: seller.name,
+                price: product.price
+            },
+            transaction
+        })
+    } catch (error) {
+        res.status(error.https_code).send(error.message)
+    }
 })
 module.exports = BuyerRouter
