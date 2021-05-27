bc285a2c97d932921ef261dde821ef8f6fe479dc
Madhav Khosla <madhav.khoslaa@gmail.com>
2021-05-27 17:50:10 +0530

    Removed reduandant stops

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,10 +5,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 
-sudo yum erase codedeploy-agent
-sudo rm -rf /opt/codedeploy-agent
-sudo ./install auto
-
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/E-Commerce"
 if [ -d "$DIR" ]; then
