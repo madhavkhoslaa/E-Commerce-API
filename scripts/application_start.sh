0698799c5cc2fb9d72079296895a8ada65b3f632
Madhav Khosla <madhav.khoslaa@gmail.com>
2021-05-27 16:59:33 +0530

    test push

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -13,6 +13,6 @@ export NVM_DIR="$HOME/.nvm"
 
 #install node modules
 npm install
-
+echo "Requirments installed"
 #start our node app in the background
 node index.js > app.out.log 2> app.err.log < /dev/null & 
