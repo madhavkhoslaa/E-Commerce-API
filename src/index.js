985d53887e3efe7b63e28de14f2de1b4037f8e9e
Madhav Khosla <madhav.khoslaa@gmail.com>
2021-05-27 19:28:29 +0530

    Removed * endpoint overloading

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
-const express = require('express')
-require('dotenv').config()
-const SellerRoute = require('./routes/seller')
-const BuyerRoute = require('./routes/buyer')
-require('./db/connection')
+const express = require("express");
+require("dotenv").config();
+const SellerRoute = require("./routes/seller");
+const BuyerRoute = require("./routes/buyer");
+require("./db/connection");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(SellerRoute)
-app.use(BuyerRoute)
-const PORT = process.env.PORT || 3003
-
-app.get('*', async (req, res) => {
-    res.status(400).send({ message: "method not allowed", 
-                        refer: 'https://github.com/madhavkhoslaa/E-Commerce-API/blob/master/README.md'})
-})
-
-app.post('*', async (req, res) => {
-    res.status(400).send({ message: "method not allowed", 
-                        refer: 'https://github.com/madhavkhoslaa/E-Commerce-API/blob/master/README.md'})
-})
+app.use(express.json());
+app.use(SellerRoute);
+app.use(BuyerRoute);
+const PORT = process.env.PORT || 3003;
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${process.env.PORT}`)
-})
+  console.log(`Server running on ${process.env.PORT}`);
+});
