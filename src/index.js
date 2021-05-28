fad06beceee5c0a60f9b9b60f2aada4351520ac1
Madhav Khosla <28945483+madhavkhoslaa@users.noreply.github.com>
2021-05-28 17:28:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
   res.send({
     message:
       "Welcome please refer to the API Documentation at https://github.com/madhavkhoslaa/E-Commerce-API/blob/master/README.md",
-    test: "CI/CD Working!"
+    test: "CI/CD Working, test message!"
   });
 });
 
